e0d4d4c8afc1d0a8b016eb385e34303f0184a0b6
Dongseon <elwynn94@gmail.com>
2024-06-15 09:38:20 +0900

    feat : AOP를 활용한 LoggingAspect 추가

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -2,11 +2,13 @@ package com.sparta.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class MyselectshopApplication {
 
     public static void main(String[] args) {
